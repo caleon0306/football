4c42a7bafef0ff5890eff6095567fbbeada3eb5a
Connell Leon <143096904+caleon0306@users.noreply.github.com>
2024-06-27 15:04:28 -0400

    updated root post to attempt to get user id

--- fantasy_football/main.py
+++ fantasy_football/main.py
@@ -4,7 +4,7 @@ from fastapi.responses import HTMLResponse,JSONResponse,RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing import Annotated
-
+from backend import funcs
 #app is set to FastAPI
 app = FastAPI()
 app.mount("/frontend/static", StaticFiles(directory="frontend/static"), name="static")
@@ -12,10 +12,18 @@ app.mount("/frontend/static", StaticFiles(directory="frontend/static"), name="st
 #templates is a variable to the folder named to directory
 templates = Jinja2Templates(directory="frontend/templates")
 
+#root directory used for login page
 @app.get("/")
 async def login(request:Request, loginError=""):
     return templates.TemplateResponse("login.html", context ={"request":request, "loginError":loginError})
 
+#attempt to login
+@app.post("/")
+async def login(request:Request, username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    result = login(username, password)
+    print(result)
+    
+#sign up page
 @app.get("/signUp")
 async def login(request:Request, signUpError=""):
     return templates.TemplateResponse("signUp.html", context ={"request":request, "signUpError":signUpError})
